545103ed623de421253bc0fecf42dd318534a1d0
Karthik Mathrubootham <48326992+karthik2872@users.noreply.github.com>
2023-01-21 17:15:30 -0500

    Update outputs.tf

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+/*
 
 output "network" {
   value = "${module.vpc.network}"
@@ -32,3 +33,4 @@ output "instance_name" {
 output "external_ip" {
   value = "${module.http_server.external_ip}"
 }
+*/
